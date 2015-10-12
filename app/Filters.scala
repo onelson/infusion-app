a8f6ca500a0ab9f90516fbc9b8cd87b5da96eb32
Owen Nelson <onelson@gmail.com>
2015-10-11 21:48:39 -0700

    adding gzip filter

--- app/Filters.scala
+++ app/Filters.scala
@@ -2,8 +2,9 @@ import javax.inject.Inject
 
 import play.api.http.HttpFilters
 import play.filters.cors.CORSFilter
+import play.filters.gzip.GzipFilter
 
 
-class Filters @Inject() (corsFilter: CORSFilter) extends HttpFilters {
-  def filters = Seq(corsFilter)
+class Filters @Inject() (corsFilter: CORSFilter, gzipFilter: GzipFilter) extends HttpFilters {
+  def filters = Seq(corsFilter, gzipFilter)
 }
